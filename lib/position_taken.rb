4e9989a6b33940c314d1dbb26d95401eb702b1af
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-20 18:10:21 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
   # check if board - index is empty -> " " or ""
-  if (board[index] == " " || board[index] == "" || board[index] == nil)
-    false
-  else
-    true
-  end
+  # ternary solution:
+  (board[index] == " " || board[index] == "" || board[index] == nil) ?
+    false : true
 end
