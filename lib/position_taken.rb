a4b958f9dca293ef83422f8bc63c89fb0fefe722
Ronald Stephens <rstephens@bluemorpho.cc>
2019-12-01 00:22:15 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,5 @@ def position_taken? (board, index)
   # check if board - index is empty -> " " or ""
   # ternary solution:
   (board[index] == " " || board[index] == "" || board[index] == nil) ?
-    false : true
+    false : false
 end
